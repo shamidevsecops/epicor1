14effdf80fc9baf4509441a50756795c42ced4f6
Shami2810 <shami.ssk2810@gmail.com>
2026-01-23 10:25:09 +0530

    RoboShop codes

--- 03.loops/sample.tf
+++ 03.loops/sample.tf
@@ -1,3 +1,15 @@
 resource "null_resource" "dummy" {
   count = 10
 }
+
+provider "aws" {}
+
+resource "aws_instance" "web" {
+  count = 2
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "web"
+  }
+}
